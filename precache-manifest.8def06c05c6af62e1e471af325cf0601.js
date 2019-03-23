86a95af6d78fd7f3d5ae34dac124fd6dd8add449
ryan emslie <ryan.emslie@gmail.com>
2019-03-22 20:36:04 -0400

    Updates

---
+++ precache-manifest.8def06c05c6af62e1e471af325cf0601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trash-client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "783bb164fa341872b2ce",
+    "url": "/trash-client/static/js/runtime~main.3046d327.js"
+  },
+  {
+    "revision": "08f73afe76d12030afef",
+    "url": "/trash-client/static/js/main.b09cafa9.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/trash-client/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "08f73afe76d12030afef",
+    "url": "/trash-client/static/css/main.a4ad4005.chunk.css"
+  },
+  {
+    "revision": "065f7f08b9c30a5c5d25a9fc748536ff",
+    "url": "/trash-client/index.html"
+  }
+];
